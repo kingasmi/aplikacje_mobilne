b586b3ed020edce244af12253cd3be38ccafb3c2
kingasmi <kinga.smialek@interia.pl>
2023-06-16 12:17:35 +0200

    jetnote4

--- jetnote3/app/src/main/java/com/example/jetnote3/util/utils.kt
+++ jetnote3/app/src/main/java/com/example/jetnote3/util/utils.kt
@@ -1,12 +1,11 @@
 package com.example.jetnote3.util
 
-import com.example.jetnote3.model.Note
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun formatDate(time: Note): String {
+fun formatDate(time: Long): String {
     val date = Date(time)
-    val format = SimpleDateFormat("EE, d MMM hh:mm aaa", Locale.getDefault())
+    val format = SimpleDateFormat("EEE, d MMM hh:mm aaa", Locale.getDefault())
     return format.format(date)
 }
